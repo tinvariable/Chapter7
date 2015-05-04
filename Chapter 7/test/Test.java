29daddb2206357923ae22db13a224d129c88ae02
Zach <tinvariable578@gmail.com>
2015-05-03 21:33:10 -0600

    Exercises Done

--- Chapter 7/test/Test.java
+++ Chapter 7/test/Test.java
@@ -3,30 +3,14 @@ package test;
 public class Test 
 {
 	
-	/*public static float sumArray()
+	public static void llIC(int[] employeeNum, String[] hireDate, double raiseAmount)
 	{
-		float[] array = {(float)12.69, (float)10.4568};
-		float num = 0;
-		float num2 = 0;
-		for(int index = 0; index <= array.length; index++)
-		{
-			if(index == 0)
-			{
-				num = index;
-			}
-			else
-			{
-				num2 = index;
-			}
-		}
-		return num + num2;
 		
-//EX 7.8		
-	}*/
-
+	}
 	public static void main(String[] args) 
 	{
-		//System.out.println(sumArray());
+	
+		
 		
 		
 		
