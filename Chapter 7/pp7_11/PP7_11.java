8c4efed8be0db9a5aa43bca8ed8fe3cc6c08b629
Zach <tinvariable578@gmail.com>
2015-05-10 11:31:23 -0600

    more

--- Chapter 7/pp7_11/PP7_11.java
+++ Chapter 7/pp7_11/PP7_11.java
@@ -3,7 +3,7 @@ package pp7_11;
 public class PP7_11 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 
 	}
 
--- Chapter 7/pp7_2/PP7_2.java
+++ Chapter 7/pp7_2/PP7_2.java
@@ -11,6 +11,7 @@ public class PP7_2
 
 	public static void main(String[] args) 
 	{
+		
 		int[] numbers = new int[51];
 		  inputArray(numbers);
 
@@ -19,12 +20,11 @@ public class PP7_2
 		  public static void inputArray(int[] myList)
 		  {
 		    Scanner input = new Scanner(System.in);
-		    
 		      System.out.print("Enter integers from -25 - 25 (input 0 value to end inputs): ");
 		      int index = 0;
-		      for(int i = 0; i < myList.length - 1; i++)
+		      for(int i = 1; i < myList.length - 1; i++)
 		      {
-		        if(i >= 0)
+		        if(i > 0)
 		        {
 		          index = input.nextInt();  
 		          if (index > 0 && index < myList.length)
@@ -34,18 +34,10 @@ public class PP7_2
 		        }
 
 		        if(myList[index-1] > 1)
-		        {
 		          System.out.println(index + " occurs " + myList[index-1] + " times ");
-		        }
 		        else
-		        {
 		          System.out.println(index + " occurs " + myList[index-1] + " time ");
-		        }
 		      } 
 		  }
 
-		
-
-	}
-
-
+}
